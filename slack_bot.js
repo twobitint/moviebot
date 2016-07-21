c6dfbb64741299a4b765beb3ccc7235aba35fe30
Justin Labak <jtlabak@gmail.com>
2016-07-20 23:33:56 -0700

    Using request instead of moviedb. integration working with tmdb

--- slack_bot.js
+++ slack_bot.js
@@ -25,50 +25,39 @@ This bot demonstrates many of the core features of Botkit:
 
   Run your bot from the command line:
 
-    token=<MY TOKEN> node slack_bot.js
+    SLACK_TOKEN=<MY TOKEN> node slack_bot.js
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
+require('dotenv').config();
 
-if (!process.env.token) {
-    console.log('Error: Specify token in environment');
+if (!process.env.SLACK_TOKEN) {
+    console.log('Error: Specify slack token in .env file');
+    process.exit(1);
+}
+
+if (!process.env.MOVIEDB_API_KEY) {
+    console.log('Error: Specify moviedb api key in .env file');
     process.exit(1);
 }
 
 var Botkit = require('botkit');
 var os = require('os');
-var http = require('http');
+var request = require('request');
+var dateFormat = require('dateFormat');
+
+var configuration = null;
+moviedb('/configuration', {}, function (res) {
+    configuration = res;
+});
 
 var controller = Botkit.slackbot({
     debug: true
 });
 
 var bot = controller.spawn({
-    token: process.env.token
+    token: process.env.SLACK_TOKEN
 }).startRTM();
-
-
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
-
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
-    });
-});
 
 controller.hears(['[“"](.*?)[”"]'], 'direct_message,direct_mention,mention', function (bot, message) {
     var search = message.match[1];
@@ -80,166 +69,139 @@ controller.hears(['(.*)'], 'direct_message,direct_mention,mention', function (bo
     imdb(bot, message, search);
 });
 
-function imdb(bot, message, search) {
-    var URL = 'http://www.imdb.com/xml/find?json=1&nr=1&q='+encodeURIComponent(search);
-    http.request(URL, function (response) {
-        var str = '';
-        response.on('data', function (chunk) {
-            str += chunk;
-        });
-        response.on('end', function () {
-            var results = JSON.parse(str);
-            var popular = results.title_popular[0];
-            var id = popular.id;
-            var type = id.substring(0, 2);
-
-            if (type == 'tt') {
-                replyMovie(bot, message, id, search);
-            } else if (type == 'nm') {
-                replyActor(bot, message, id, search);
-            }
-        });
-    }).end();
+function moviedb(path, options, callback) {
+    var api = 'https://api.themoviedb.org/3';
+    options.api_key = process.env.MOVIEDB_API_KEY;
+    request({
+        url: api + path,
+        qs: options
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            callback(JSON.parse(body));
+        }
+    });
 }
 
-function replyMovie(bot, message, id, search) {
-    http.request('http://app.imdb.com/title/maindetails?tconst='+id, function (response) {
-        var str = '';
-        response.on('data', function (chunk) {
-            str += chunk;
-        });
-        response.on('end', function () {
-            var data = JSON.parse(str).data;
+function imdb(bot, message, search) {
+    // var URL = 'http://www.imdb.com/xml/find?json=1&nr=1&q='+encodeURIComponent(search);
+    // http.request(URL, function (response) {
+    //     var str = '';
+    //     response.on('data', function (chunk) {
+    //         str += chunk;
+    //     });
+    //     response.on('end', function () {
+    //         var results = JSON.parse(str);
+    //         var popular = results.title_popular[0];
+    //         var id = popular.id;
+    //         var type = id.substring(0, 2);
+    //
+    //         if (type == 'tt') {
+    //             replyMovie(bot, message, id, search);
+    //         } else if (type == 'nm') {
+    //             replyActor(bot, message, id, search);
+    //         }
+    //     });
+    // }).end();
 
-            var thumb = data.image.url.replace('.jpg', 'UX182_CR0,0,75,75.jpg');
+    moviedb('/search/multi', {query: search}, function (res) {
+        if (res.results.length != 0) {
+            var result = res.results[0];
+            if (result.media_type == 'movie') {
+                replyMovie(bot, message, search, result);
+            } else if (result.media_type == 'person') {
+                replyPerson(bot, message, search, result);
+            } else if (result.media_type == 'tv') {
+                //
+            }
+        }
+    });
+}
 
+function replyMovie(bot, message, search, movie) {
+
+    moviedb('/movie/'+movie.id, {}, function (info) {
+        moviedb('/movie/'+movie.id+'/credits', {}, function (credits) {
+            var year = info.release_date.substring(0, 4);
+            var thumb = configuration.images.base_url
+                + configuration.images.poster_sizes[0]
+                + info.poster_path;
+            var release = dateFormat(info.release_date, 'fullDate');
             bot.reply(message, {
-                response_type: 'in_channel',
+                response_type: 'ephemeral',
                 text: 'This is what I found for “' + search + '”',
                 attachments: [
                     {
-                        title: data.title + ' (' + data.year + ')',
-                        title_link: 'http://www.imdb.com/title/'+id,
+                        title: info.original_title + ' (' + year + ')',
+                        title_link: 'http://www.imdb.com/title/'+info.imdb_id,
                         thumb_url: thumb,
-                        text: data.best_plot.outline + ' ' + thumb,
+                        text: info.overview,
                         fields: [
                             {
                                 title: 'Released',
-                                value: data.release_date.normal,
+                                value: release,
                                 short: true
                             },
                             {
                                 title: 'Runtime',
-                                value: (data.runtime.time / 60) + ' min ' + data.runtime.attr,
+                                value: (info.runtime) + ' min',
                                 short: true
                             },
                             {
-                                title: 'Actors',
-                                value: data.cast_summary.map(function (cur) {
-                                    return cur.name.name;
+                                title: 'Cast',
+                                value: credits.cast.slice(0, 4).map(function (cur) {
+                                    return cur.name;
                                 }).join(', '),
                                 short: true
                             },
                             {
-                                title: 'Rating',
-                                value: data.rating + '/10 ('+data.num_votes+')',
+                                title: 'TMDB Rating',
+                                value: info.vote_average + '/10 (' + info.vote_count + ')',
                                 short: true
+                            }
+                        ]
+                    },
+                    {
+                        title: 'Do you want to interact with my buttons?',
+                        callback_id: '123',
+                        attachment_type: 'default',
+                        actions: [
+                            {
+                                "name":"yes",
+                                "text": "Yes",
+                                "value": "yes",
+                                "type": "button",
+                            },
+                            {
+                                "name":"no",
+                                "text": "No",
+                                "value": "no",
+                                "type": "button",
                             }
                         ]
                     }
                 ]
             });
         });
-    }).end();
+    });
+
+    // http.request('http://app.imdb.com/title/maindetails?tconst='+id, function (response) {
+    //     var str = '';
+    //     response.on('data', function (chunk) {
+    //         str += chunk;
+    //     });
+    //     response.on('end', function () {
+    //         var data = JSON.parse(str).data;
+    //
+    //         var thumb = data.image.url.replace('.jpg', 'UX182_CR0,0,75,75.jpg');
+    //
+    //
+    //     });
+    // }).end();
 }
 
-function replyActor(bot, message, id) {
+function replyPerson(bot, message, search, person) {
 
 }
-
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var name = message.match[1];
-    controller.storage.users.get(message.user, function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
-});
-
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
-
-
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
-            });
-        }
-    });
-});
-
 
 controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
 
